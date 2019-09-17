07e86675d1472595f3df52ba18143acee4553b87
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-09-17 13:21:48 +0100

    part 1 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+import { Route } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
-const App = () => {
+const App = (props) => {
   const [savedList, setSavedList] = useState( [] );
-
+  
+// const id = props;
+// debugger
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
   };
@@ -12,7 +17,22 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
+
+      
+      {/* <Route path='/movies/:id' render={props => {
+        // 1- find out what this ":id" actually is in the browser location
+        const currentAppParameter = props.match.params.id;
+        // 2- knowing which app, pull the right component from the apps array
+        const theRightAppObject = id.find(appObject => {
+          return appObject.url === currentAppParameter;
+        });
+        const Component = theRightAppObject.id;
+        // 3- return the <Component />
+        return <Component {...props} name='gabe' />;
+      }} /> */}
     </div>
   );
 };
